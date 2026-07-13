bca03361b6eafa78f01d8b8db3b735993d3da421
ishajainbtech24-oss <ishajainbtech24@rvu.edu.in>
2026-07-13 17:39:43 +0000

    frontend updated

--- app/app.py
+++ app/app.py
@@ -1,50 +1,25 @@
-from utils.predict import predict_flood
-from flask import Flask, render_template, request, send_from_directory
-import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = "uploads"
-app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
-
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-
-
-@app.route("/", methods=["GET", "POST"])
-def index():
-
-    if request.method == "POST":
-
-        file = request.files["image"]
-
-        if file.filename == "":
-            return render_template("index.html")
-
-        upload_path = os.path.join(
-            app.config["UPLOAD_FOLDER"],
-            file.filename
-        )
-
-        file.save(upload_path)
-
-        result = predict_flood(upload_path)
-
-        return render_template(
-            "result.html",
-            image=file.filename,
-            prediction=result["label"],
-            confidence=result["confidence"]
-        )
-
+@app.route("/")
+def home():
     return render_template("index.html")
 
 
-@app.route("/uploads/<filename>")
-def uploaded_file(filename):
-    return send_from_directory(
-        app.config["UPLOAD_FOLDER"],
-        filename
-    )
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+
+@app.route("/upload")
+def upload():
+    return render_template("upload.html")
+
+
+@app.route("/map")
+def map_view():
+    return render_template("map.html")
 
 
 if __name__ == "__main__":
